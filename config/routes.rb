bf18f5699caaee809ac7582897a0812438bdaa95
naokikubo2 <naoki16871@gmail.com>
2020-09-30 08:28:23 +0900

    add homes_top #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'homes/top'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
   }
 
   devise_scope :user do
+    root :to => "homes#top"
     get "signup", :to => "users/registrations#new"
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
