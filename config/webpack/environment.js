93d6bbe2a21f93f011b637532c2f51098db2a083
naokikubo2 <naoki16871@gmail.com>
2020-10-06 23:37:14 +0900

    指摘事項修正 #2

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,4 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
-
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-
-// Vue.js フル版（Compiler入り）
-environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
 
 // jQueryとBootstapのJSを使えるように
 const webpack = require('webpack')
