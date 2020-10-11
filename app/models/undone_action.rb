f709efaac0212c69d84a973332b11fcb3567acb7
naokikubo2 <naoki16871@gmail.com>
2020-10-11 11:00:27 +0900

    add undone_action_model #4

--- app/models/undone_action.rb
+++ app/models/undone_action.rb
@@ -1,3 +1,6 @@
 class UndoneAction < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :undone_action, presence: true, length: { maximum: 25 }, uniqueness: true
+  validates :default_time, presence: true, length: { maximum: 4 }
 end
