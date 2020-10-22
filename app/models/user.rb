b3b8fcb4f7d6ea002630aa04dbf220ecaa73f682
naokikubo2 <naoki16871@gmail.com>
2020-10-22 20:46:27 +0900

    add savings_record model #3

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :undone_actions, dependent: :destroy
+  has_many :savings_records, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_140946) do
+ActiveRecord::Schema.define(version: 2020_10_21_134712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "savings_records", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id", null: false
     t.integer "earned_time"
-    t.date "date"
+    t.date "savings_date"
+    t.string "savings_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_savings_records_on_user_id"
   end
 
   create_table "undone_actions", force: :cascade do |t|
@@ -45,5 +47,6 @@ ActiveRecord::Schema.define(version: 2020_10_07_140946) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "savings_records", "users"
   add_foreign_key "undone_actions", "users"
 end
