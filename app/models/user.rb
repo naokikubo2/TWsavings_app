27c61f0a30148aaa9efd72a95619d1f9cedda001
naokikubo2 <naoki16871@gmail.com>
2020-11-18 22:45:43 +0900

    modify User_model_validate #41

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :hourly_pay, presence: true, length: { maximum: 6 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20}
 
   has_many :undone_actions, dependent: :destroy
   has_many :savings_records, dependent: :destroy
